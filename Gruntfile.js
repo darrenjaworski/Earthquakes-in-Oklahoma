243dd7fe6a05c9681453e5d10cfce1eee6482bff
Darren Jaworski <darrenjaworski@gmail.com>
2014-02-18 20:23:52 -0600

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,58 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      dist: {
+        files: {
+          'assets/js/scripts.min.js': [
+          'assets/js/lib/*.js',
+          //'assets/js/_*.js'
+		  ]
+		}
+	  }		  
+	},
+    sass: {
+	  dist: {
+	    options: {
+	      style: 'compressed',
+	      compass: 'true',
+	      sourcemap: false
+	    },
+	    files: {
+		    'assets/css/screen.css' : [
+		    'assets/sass/screen.scss'
+		   ]
+	    }
+	  }
+	},
+	watch: {
+	  options: {
+		livereload: true
+      },
+	  sass: {
+		files: [
+		  'assets/sass/*.scss',
+		  'assets/sass/partials/*.scss'
+		],
+		tasks: [
+		  'sass'
+		], 
+	  }
+    },
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+
+  // Default task(s).
+  grunt.registerTask('default', [
+  	'uglify',
+  	'sass',
+  	'watch'
+  	]);
+
+};
---
+++ assets/js/script.js
@@ -0,0 +1,77 @@
+
+$(document).ready( function()	{
+	
+	var width = parseInt($('.map').css('width')),
+		height = width * 0.5,
+		active;
+		
+	var projection = d3.geo.mercator()
+	    .center([-98, 35])
+	    .scale(6400)
+	    .translate([575, 300])
+	    .precision(0);
+	    
+	var scale = d3.scale.log()
+		.domain([1, 5.6])
+		.range([1,5]);
+	
+	var path = d3.geo.path().projection(projection);
+	
+	var svg = d3.select(".map")
+		.append("svg")
+		.style("width", width)
+		.style("height", height);
+		
+	var g = svg.append("g");
+	
+	d3.json("assets/data/combined.json", function(error, data) {
+	
+		var counties = data.objects.counties;
+	
+		var county = g.append("g")
+			.attr("class", "counties")
+			.selectAll("path")
+			.data(topojson.feature(data, counties).features)
+			.enter()
+			.append("path")
+			.attr("class", "county")
+			.attr("d", path)
+			.on("click", clicked);
+			
+		var earthquakes = data.objects.earthquakes;
+		    
+		var earthquake = g.append("g")
+			.attr("class", "earthquakes")
+			.selectAll("path")
+			.data(topojson.feature(data, earthquakes).features.sort( function(a, b) { return b.properties.origintime - a.properties.origintime; }))
+			.enter()
+			.append("path")
+			.attr("class", "earthquake")
+			.attr("d", path.pointRadius(0.1))
+			.transition()
+			.duration(1500)
+			.delay(function(d, i) { return i * 5; })
+			.attr("d", path.pointRadius( function(d) { if ( !d.properties.magnitude || d.properties.magnitude < 2.5 ) { return 1 } else { return scale(d.properties.magnitude); } } ));	
+			   
+	});
+      
+	function clicked(d) {
+		if (active === d) return reset();
+		g.selectAll(".active").classed("active", false);
+		d3.select(this).classed("active", active = d);
+		
+		var b = path.bounds(d);
+		g.transition()
+			.duration(750)
+			.attr("transform",
+			"translate(" + projection.translate() + ")"
+			+ "scale(" + .85 / Math.max((b[1][0] - b[0][0]) / width, (b[1][1] - b[0][1]) / height) + ")"
+			+ "translate(" + -(b[1][0] + b[0][0]) / 2 + "," + -(b[1][1] + b[0][1]) / 2 + ")");
+	}
+	
+	function reset() {
+		g.selectAll(".active").classed("active", active = false);
+		g.transition().duration(750).attr("transform", "");
+	}
+	
+});
